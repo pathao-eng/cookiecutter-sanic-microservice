7784fae546651026684b71f4b770907b3bc9da86
Hasibul Hasan (Anik) <anikhasibul@outlook.com>
2019-01-15 20:36:11 +0000

    Get port from environment variable by default
    If environment variable `PORT` doesn't exist, then fallback to `8000`

--- {{cookiecutter.github_repository_name}}/source/{{cookiecutter.app_name}}/app.py
+++ {{cookiecutter.github_repository_name}}/source/{{cookiecutter.app_name}}/app.py
@@ -1,7 +1,11 @@
 from sanic import Sanic
+import os
 
 app = Sanic()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = os.environ['PORT']
+    if port == "":
+        port = 8000
+    app.run(host="0.0.0.0", port=int(port))
