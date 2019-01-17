dab13daa2da585132425e3a16036e5a6dd3771a5
Tahzib Mashrik <mashrikt@gmail.com>
2019-01-18 05:33:46 +0600

    setup sentry

--- {{cookiecutter.github_repository_name}}/source/{{cookiecutter.app_name}}/app.py
+++ {{cookiecutter.github_repository_name}}/source/{{cookiecutter.app_name}}/app.py
@@ -1,5 +1,15 @@
+import sentry_sdk
 from sanic import Sanic
 from sanic.response import json
+from sentry_sdk.integrations.sanic import SanicIntegration
+
+from .config import SENTRY_DSN
+
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[SanicIntegration()]
+)
 
 app = Sanic()
 
@@ -7,6 +17,7 @@ app = Sanic()
 @app.route("/")
 async def welcome_view(request):
     return json({"message": "Welcome to {{cookiecutter.app_name}} API"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
--- {{cookiecutter.github_repository_name}}/source/{{cookiecutter.app_name}}/config.py
+++ {{cookiecutter.github_repository_name}}/source/{{cookiecutter.app_name}}/config.py
@@ -1,1 +1,4 @@
+from decouple import config
 
+
+SENTRY_DSN = config('SENTRY_DSN', default='')
